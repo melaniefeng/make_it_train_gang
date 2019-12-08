daee4db1cbea32a6bc814c2e61d0263ab01658c8
Melanie Feng <melanie.feng@gmail.com>
2019-12-08 14:32:27 -0600

    relative location factors into unlocking

---
+++ controller_location.py
@@ -0,0 +1,112 @@
+
+# coding: utf-8
+
+# In[36]:
+
+
+print("\n\nWelcome to controller\n")
+
+password = None
+passwordIndex = 0
+errorCount = 0
+prevGesture = [-1, -1, -1]
+prevInputGesture = [-1, -1, -1]
+passwordLoc = None
+def unlock(predGesture):
+    global passwordLoc
+    global passwordIndex
+    global errorCount
+    global prevGesture
+    global prevInputGesture
+    if passwordIndex == len(password):
+        print('complete')
+    else:
+            
+        nextGesture = passwordLoc[passwordIndex][0]
+        nextGestureUL_x = passwordLoc[passwordIndex][1]
+        nextGestureUL_y = passwordLoc[passwordIndex][2]
+        print(passwordLoc[passwordIndex])
+      
+        shiftCheck = False
+        shiftDir = False
+        if prevInputGesture[0] == -1 or prevGesture[0] == -1:
+            prevInputGesture = predGesture
+            shiftCheck = True
+            shiftDir = True
+        else:
+            expShift_x = prevGesture[1]-nextGestureUL_x
+            expShift_y = prevGesture[2]-nextGestureUL_y
+            
+            print('expected shift', expShift_x, expShift_y)
+            currShift_x = prevInputGesture[1]-predGesture[1]
+            currShift_y = prevInputGesture[2]-predGesture[2]
+            print('current shift', currShift_x, currShift_y)
+            
+            if expShift_x/abs(expShift_x) == currShift_x/abs(currShift_x):
+                if expShift_y/abs(expShift_y) == currShift_y/abs(currShift_y):
+                    print('shiftDir')
+                    shiftDir = True
+            #threshold to check if movement is okay
+            if currShift_x <= expShift_x+abs(expShift_x)*.1 and currShift_x >= expShift_x-abs(expShift_x)*.1:
+                print('threshold:',abs(expShift_x)*.1)
+                print('UL ok')
+                if currShift_y <= expShift_y+abs(expShift_y)*.1 and currShift_y >= expShift_y-abs(expShift_y)*.1:
+                    print('BR ok')
+                    shiftCheck = True
+                
+        if predGesture[0] == nextGesture and shiftCheck:
+            #gesture ID matches, check relative location
+            print('check1', predGesture)
+            prevGesture = passwordLoc[passwordIndex]
+            prevInputGesture = predGesture
+            passwordIndex = passwordIndex + 1
+            if passwordIndex == len(password):
+                print('opened')
+                reset()
+                
+        elif not predGesture[0] == prevGesture[0] or not shiftDir:
+            if passwordIndex != 0:
+                errorCount = errorCount + 1
+            print('incorrect')
+            if errorCount > 5:
+                reset()
+        
+def reset():
+    global passwordIndex
+    global errorCount
+    global prevGesture
+    global prevInputGesture
+    print('reset')
+    passwordIndex = 0
+    errorCount = 0
+    prevGesture = [-1, -1, -1]
+    prevInputGesture = [-1, -1, -1]
+
+def setPassword():
+    pass
+    #read pfrom sean script
+    
+    #write to password.txt array
+    #hold out hand
+    #take10 pic
+
+def initPassword():
+    global password
+    global passwordLoc
+    keyFile = open('pwd.txt', 'r')
+    password = keyFile.read().splitlines()
+    passwordLoc = []
+    for c in password:
+        d = c.split(" ")
+        d[1] = int(d[1])
+        d[2] = int(d[2])
+        passwordLoc.append(d)
+    print(passwordLoc)
+
+
+initPassword()
+testInput = [['cat', 0, 200], ['bird', 50, 1000], ['bird', 0, 900]]
+
+for i in testInput:
+    unlock(i)
+
--- main.py
+++ main.py
@@ -1,26 +1,61 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[48]:
 
 
 import subprocess
 import os
 from parse import PredictionParser
-import controller
+
+import controller_location
 
 
-# In[4]:
+# In[51]:
+
+
+import imp
+imp.reload(controller_location)
+print(controller_location.passwordLoc)
+controller_location.reset()
+testInput = [['cat', 0, 200], ['bird', 100, 1000], ['bird', 50, 900]]
+
+for i in testInput:
+    controller_location.unlock(i)
+
+
+# In[32]:
 
 
 threshold = .6
-controller.initPassword()
-print(controller.password)
+
+
+# In[25]:
+
+
+password = []
+for c in controller.password:
+    d = c.split(" ")
+    passwordID.append(d[0])
+    d[1] = int(d[1])
+    d[2] = int(d[2])
+    password.append(d)
+
+print(password)
+
+
+# In[52]:
+
+
+controller_location.reset()
+print(controller_location.password)
 for filename in os.listdir('input_images'):
     img = 'input_images/' + filename
     output = subprocess.Popen(['./darknet', 'detect', 'cfg/yolov2.cfg', 'yolov2.weights', img], stdout = subprocess.PIPE)
     results = PredictionParser().parse_preds(output.communicate()[0], threshold)
     print(results)
     if len(results) > 0:
-        controller.unlock(results[0]['class'])
+        query = [results[0]['class'], int(results[0]['coords'][0][0]), int(results[0]['coords'][0][1])]
+        print(query)
+        controller_location.unlock(query)
 
