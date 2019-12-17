9f8c8a572011402a6ffc8d5bb1c3d2846c64babc
Melanie Feng <melanie.feng@gmail.com>
2019-12-16 23:00:07 -0600

    main

--- main.py
+++ main.py
@@ -1,61 +1,58 @@
 
 # coding: utf-8
 
-# In[48]:
+# In[4]:
 
 
 import subprocess
 import os
 from parse import PredictionParser
-
+import wget
 import controller_location
+import playVideo
+import controller
 
 
-# In[51]:
+# In[5]:
 
 
 import imp
-imp.reload(controller_location)
-print(controller_location.passwordLoc)
-controller_location.reset()
-testInput = [['cat', 0, 200], ['bird', 100, 1000], ['bird', 50, 900]]
+imp.reload(controller)
+controller.reset()
+testInput = [['longhorn', 0, 200], ['longhorn', 0, 200], ['L', 100, 1000],['L', 100, 1000],['L', 100, 1000], ['palm', 50, 900]]
 
 for i in testInput:
-    controller_location.unlock(i)
+    print("here", i)
+    controller.unlock(i)
 
 
-# In[32]:
+# In[6]:
 
 
-threshold = .6
+controller.initPassword()
+print(controller.password)
+threshold = .3
 
 
-# In[25]:
+# In[ ]:
 
 
-password = []
-for c in controller.password:
-    d = c.split(" ")
-    passwordID.append(d[0])
-    d[1] = int(d[1])
-    d[2] = int(d[2])
-    password.append(d)
-
-print(password)
-
-
-# In[52]:
-
-
-controller_location.reset()
-print(controller_location.password)
-for filename in os.listdir('input_images'):
-    img = 'input_images/' + filename
-    output = subprocess.Popen(['./darknet', 'detect', 'cfg/yolov2.cfg', 'yolov2.weights', img], stdout = subprocess.PIPE)
+controller.reset()
+controller.initPassword()
+print(controller.password)
+count = 0
+while(True):
+    img_name = str(count)+'.jpg'
+    print('request')
+    subprocess.Popen(['wget', '-q' ,'-O', img_name, 'http://192.168.1.33:8000/shot.jpg']).wait()
+    print('picture obtained')
+    output = subprocess.Popen(['./darknet','detector', 'test', 'cfg/data3.data','cfg/yolo_data3.cfg', 'yolo-obj_13000.weights', img_name, '-thresh', '.1'], stdout = subprocess.PIPE)
     results = PredictionParser().parse_preds(output.communicate()[0], threshold)
     print(results)
     if len(results) > 0:
         query = [results[0]['class'], int(results[0]['coords'][0][0]), int(results[0]['coords'][0][1])]
         print(query)
-        controller_location.unlock(query)
+        controller.unlock(query)
+           
+    count = count + 1
 
